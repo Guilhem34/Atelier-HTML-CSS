1d07334c4a70f71da7360422c56033e6df821daf
Guilhem <guilhempujols@gmail.com>
2024-05-07 13:54:51 +0200

    Please

--- JS/script.js
+++ JS/script.js
@@ -3,6 +3,7 @@ let icons = document.getElementById("icons");
 icons.addEventListener("click", () => {
   nav.classList.toggle("active");
 });
+
 links.forEach((link) => {
   link.addEventListener("click", () => {
     nav.classList.remove("active");
